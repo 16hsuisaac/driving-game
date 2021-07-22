36095e44079637e7c2cf7bbcf642fd4de2c45170
Isaac Hsu <16hsuisaac@gmail.com>
2021-07-22 13:27:59 -0700

    listens for spacebar down and up and has incrementing timer per ms

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 var $car = document.querySelector('.car-image');
+var timeBetween = 0;
+var count = null;
 
 document.addEventListener('keydown', arrows);
 
@@ -12,4 +14,17 @@ function arrows(event) {
   } else if (event.key === 'ArrowLeft') {
     $car.setAttribute('class', 'car-image left-arrow');
   }
+  if (event.key === ' ') {
+    timeBetween = 0;
+    count = setInterval(function () {
+      timeBetween++;
+    });
+    $car.style.left = $car.style.left.value + timeBetween + 'px';
+  }
+}
+
+document.addEventListener('keyup', timing);
+
+function timing() {
+  clearInterval(count);
 }
