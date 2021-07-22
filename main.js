e165233c1d9d88816348012b3a4a1a64598850c4
Isaac Hsu <16hsuisaac@gmail.com>
2021-07-22 15:54:30 -0700

    added variable to see which direction the car is facing, added conditionals to read direction, moves car according to direction facing

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ var count = null;
 var data = {
   xAxis: 0,
   yAxis: 0,
-  carMoving: false
+  carMoving: false,
+  carDirection: 'East'
 };
 var distance = 0;
 
@@ -12,23 +13,59 @@ document.addEventListener('keydown', arrows);
 function arrows(event) {
   if (event.key === 'ArrowUp') {
     $car.setAttribute('class', 'car-image up-arrow');
+    data.carDirection = 'North';
   } else if (event.key === 'ArrowRight') {
     $car.setAttribute('class', 'car-image right-arrow');
+    data.carDirection = 'East';
   } else if (event.key === 'ArrowDown') {
     $car.setAttribute('class', 'car-image down-arrow');
+    data.carDirection = 'South';
   } else if (event.key === 'ArrowLeft') {
     $car.setAttribute('class', 'car-image left-arrow');
+    data.carDirection = 'West';
   }
-  if (event.key === ' ' && data.carMoving === false) {
 
+  if (event.key === ' ' && data.carMoving === false) {
     data.carMoving = true;
+  } else if (event.key === ' ' && data.carMoving === true) {
+    clearInterval(count);
+    data.carMoving = false;
+  }
+  if (data.carMoving === true && (event.key === 'ArrowRight' || data.carDirection === 'East')) {
+    distance = data.xAxis;
+    clearInterval(count);
     count = setInterval(function () {
+      $car.style.top = data.yAxis + 'px';
       $car.style.left = (parseInt(data.xAxis) + parseInt(5)) + 'px';
       distance = distance + 5;
       data.xAxis = distance;
     }, 16);
-  } else if (event.key === ' ' && data.carMoving === true) {
+  } else if (data.carMoving === true && (event.key === 'ArrowLeft' || data.carDirection === 'West')) {
     clearInterval(count);
-    data.carMoving = false;
+    distance = data.xAxis;
+    count = setInterval(function () {
+      $car.style.top = data.yAxis + 'px';
+      $car.style.left = (parseInt(data.xAxis) - parseInt(5)) + 'px';
+      distance = distance - 5;
+      data.xAxis = distance;
+    }, 16);
+  } else if (data.carMoving === true && (event.key === 'ArrowDown' || data.carDirection === 'South')) {
+    clearInterval(count);
+    distance = data.yAxis;
+    count = setInterval(function () {
+      $car.style.left = data.xAxis + 'px';
+      $car.style.top = (parseInt(data.yAxis) + parseInt(5)) + 'px';
+      distance = distance + 5;
+      data.yAxis = distance;
+    }, 16);
+  } else if (data.carMoving === true && (event.key === 'ArrowUp' || data.carDirection === 'North')) {
+    clearInterval(count);
+    distance = data.yAxis;
+    count = setInterval(function () {
+      $car.style.left = data.xAxis + 'px';
+      $car.style.top = (parseInt(data.yAxis) - parseInt(5)) + 'px';
+      distance = distance - 5;
+      data.yAxis = distance;
+    }, 16);
   }
 }
