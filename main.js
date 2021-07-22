d66195664117ae6c07c13c7c8c7e3ad74be66376
Isaac Hsu <16hsuisaac@gmail.com>
2021-07-22 14:59:02 -0700

    added bool to data model, added conditional to space bar event listener, cleared interval if car was moving

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 var $car = document.querySelector('.car-image');
-/* var timeBetween = 0;
-var count = null; */
+var count = null;
 var data = {
   xAxis: 0,
-  yAxis: 0
+  yAxis: 0,
+  carMoving: false
 };
+var distance = 0;
 
 document.addEventListener('keydown', arrows);
 
@@ -18,19 +19,16 @@ function arrows(event) {
   } else if (event.key === 'ArrowLeft') {
     $car.setAttribute('class', 'car-image left-arrow');
   }
-  if (event.key === ' ') {
-    var distance = 0;
-    /* count =  */setInterval(function () {
-      $car.style.left = (parseInt(distance) + parseInt(5)) + 'px';
+  if (event.key === ' ' && data.carMoving === false) {
+
+    data.carMoving = true;
+    count = setInterval(function () {
+      $car.style.left = (parseInt(data.xAxis) + parseInt(5)) + 'px';
       distance = distance + 5;
       data.xAxis = distance;
     }, 16);
+  } else if (event.key === ' ' && data.carMoving === true) {
+    clearInterval(count);
+    data.carMoving = false;
   }
 }
-
-/* document.addEventListener('keyup', timing);
-
-function timing() {
-  clearInterval(count);
-}
- */
